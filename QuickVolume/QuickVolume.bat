3c4e3c19b6c97014ddec67d3da15b2bda3f0ea93
Jmcgee1125 <jmcgee01125@gmail.com>
2022-02-16 20:56:19 -0500

    quickvolume - change audio volume without re-encoding video

---
+++ QuickVolume/QuickVolume.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: -------------------------------------
+
+:: QuickVolume Version 1.0
+
+:: -------------------------------------
+
+:: USAGE: Drag a video file onto this program and type a percentage to change the volume
+
+:: -------------------------------------
+
+:: Code
+
+title QuickAudio
+
+:: if the user didn't give a file, show an error
+if [%1]==[] goto ERROR_file
+
+:: get user selection for audio
+echo Type a percentage of audio (10%%, 125%%, etc.), but leave out the %% sign (e.g. 125), then press enter.
+set /p volume=
+
+:: change the audio level
+:: ffmpeg -input filename -codec:video copy -filter:audio "volume=volumelevel" outputname
+ffmpeg -i "%~f1" -c:v copy -filter:a "volume=%volume%/100" "%~n1_qa%~x1"
+exit
+
+:ERROR_file
+echo Error: please drag a file onto this program to use it.
+pause
+exit
---
+++ QuickVolume/README.txt
@@ -0,0 +1,18 @@
+
+:: -------------------------------------
+
+QuickVolume Version 1.0
+
+:: -------------------------------------
+
+QuickVolume allows changing the audio volume of a video file without re-encoding the whole thing.
+
+:: -------------------------------------
+
+To use, drag the video file onto QuickVolume.bat, then type a percentage to scale the volume by (but leave out the percent sign, e.g. "125" for 125%).
+
+The output will be saved as <source file>_qa.<source extension>
+
+:: -------------------------------------
+
+There are no variables inside of QuickVolume.bat
