e17110aac116cd9f0bdc86a52b725a3d9862347b
Jmcgee1125 <jmcgee01125@gmail.com>
2022-09-17 23:46:44 -0400

    fix negative detection error

--- QuickCrop/QuickCrop.bat
+++ QuickCrop/QuickCrop.bat
@@ -2,7 +2,7 @@
 
 :: -------------------------------------
 
-:: QuickCrop Version 1.0
+:: QuickCrop Version 1.0b
 
 :: -------------------------------------
 
@@ -19,23 +19,32 @@ set ConfirmResolution=1
 
 :: Code
 
+echo Detecting original resolution...
+
+for /f "usebackq delims=" %%a in (`ffprobe -v error -select_streams v:0 -show_entries stream^=width -of default^=noprint_wrappers^=1:nokey^=1 "%~f1"`) do set srcwidth=%%a
+for /f "usebackq delims=" %%a in (`ffprobe -v error -select_streams v:0 -show_entries stream^=height -of default^=noprint_wrappers^=1:nokey^=1 "%~f1"`) do set srcheight=%%a
+echo Extracted source width (%srcwidth%) and height (%srcheight%)
+
 echo Detecting target resolution...
 
 :: converted from bash - https://gist.github.com/schocco/21981bc00c37c851e3ca
-for /F "tokens=2 delims==" %%A in ('ffmpeg -hide_banner -i %1 -t 1 -vf cropdetect -f null null 2^>^&1') do (
-	echo %%A | find ":" >nul
-	if errorlevel 1 (echo.>nul) else (set croptarget=%%A)
+for /F "tokens=2 delims==" %%a in ('ffmpeg -hide_banner -i %1 -t 1 -vf cropdetect -f null null 2^>^&1') do (
+	echo %%a | find ":" >nul
+	if errorlevel 1 (echo.>nul) else (set croptarget=%%a)
 )
 echo Extracted crop information: %croptarget%
 
 :: extract the width and height
-for /f "tokens=1 delims=:" %%A in ("%croptarget%") do set width=%%A
-for /f "tokens=2 delims=:" %%A in ("%croptarget%") do set height=%%A
+for /f "tokens=1 delims=:" %%a in ("%croptarget%") do set width=%%a
+for /f "tokens=2 delims=:" %%a in ("%croptarget%") do set height=%%a
 echo Extracted width (%width%) and height (%height%)
+if %width% LSS 0 set /a width=width*-1
+if %height% LSS 0 set /a height=height*-1
+echo Adjusted width (%width%) and height (%height%)
 
 cls
 
-echo Detected output resolution %width% by %height%
+echo Detected output resolution %width% by %height% (Source is %srcwidth% by %srcheight%)
 if %ConfirmResolution%==1 (
 	choice /c YCN /m "Continue? (Yes / Change / No (cancel))"
 )
@@ -48,4 +57,4 @@ if %errorlevel%==2 (
 	set /p height=
 )
 
-ffmpeg -y -i %1 -vf "crop=%width%:%height%" %~f1_qcr%~x1
+ffmpeg -y -i %1 -vf "crop=%width%:%height%" "%~n1_qcr%~x1"
