a5024e0d4aaf2749e18febe253f95f356502ade2
Jmcgee1125 <jmcgee01125@gmail.com>
2022-11-27 14:34:45 -0500

    actually fixed audio desync Turns out this is because of how audio timestamps are parsed. When doing a QuickTrim, the audio and video is simply copy-pasted (with modified metadata) into the resulting file, for speed and quality reasons. This can cause certain timestamps to shift into negative offsets. During decode, certain cases cause FFmpeg (or Windows' viewer) to apparently shift these values to be all-positive. Hence, desync.

--- QuickTrim/QuickTrim.bat
+++ QuickTrim/QuickTrim.bat
@@ -2,7 +2,7 @@
 
 :: -------------------------------------
 
-:: QuickTrim Version 1.2c
+:: QuickTrim Version 1.2d
 
 :: -------------------------------------
 
@@ -89,7 +89,7 @@ if "%end%" NEQ "" set end=-to %end%
 goto TRIM
 
 :TRIM
-ffmpeg -ss %start% %end% -i %1 -c copy "%~n1_trim%~x1"
+ffmpeg -ss %start% %end% -i %1 -c copy -avoid_negative_ts make_zero -fflags +genpts  "%~n1_trim%~x1"
 pause
 exit
 
